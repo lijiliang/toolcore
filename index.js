9bdc8b104852ce4b71e515bbaa9feb6494f98a6f
Benson <sucaiweb@163.com>
2019-06-05 12:05:04 +0800

    feat: 利用parcel构建以UMD 方式暴露模块

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import { version } from './package.json'
+import * as obj from './libs/object'
+import * as arr from './libs/array'
 
 const toolcore = {
-  version: version
+  ...obj,
+  ...arr,
+  version: '0.1.0'
+  // version
 }
 
 for(let key in toolcore) {
---
+++ libs/array.js
@@ -0,0 +1,64 @@
+/*
+ * @Author: Benson
+ * @Date: 2019-06-05 11:25:22
+ * @LastEditors: Benson
+ * @LastEditTime: 2019-06-05 12:04:19
+ * @Description: 数组
+ */
+
+
+/**
+ * 根据属性去重数组
+ * @param {array} arr 去重的数组
+ * @param {string} key 去重的key
+ * @example toolcore.uniqueBy([{name:'1111'},{name:'1111'},{name:'222'},{name:'333'}],'name') => [{name:'1111'},{name:'222'},{name:'333'}
+ */
+export const uniqueBy = function (arr, key) {
+  return arr.filter((element, index, array) => array.findIndex(row => row[key] === element[key]) === index)
+}
+
+/**
+ * 普通数组去重
+ * @param {array} arr 去重的数组
+ * @example toolcore.unique([1,2,2,3,4,3,4,7]) => [1, 2, 3, 4, 7]
+ */
+export const unique = (arr) =>
+    arr.filter((element, index, array) => array.indexOf(element) === index)
+
+
+/**
+ * 找出数组中该属性最大值的一列
+ * @param {array} arr 
+ * @param {string} key 
+ * @example toolcore.maxNumBy([{num:55},{num:541},{num:41}],'num') // => {num: 541}
+ */
+export const maxNumBy = (arr, key) =>
+    arr.find(item => item[key] === Math.max.apply(Math, arr.map(row => row[key])))
+
+
+/**
+ * 找出数组中该属性最小值的一列
+ * @param {array} arr 
+ * @param {string} key 
+ * @example toolcore.minNumBy([{num:55},{num:541},{num:41}],'num') // =>  {num: 41}
+ */
+export const minNumBy = (arr, key) =>
+    arr.find(item => item[key] === Math.min.apply(Math, arr.map(row => row[key])))
+
+
+/**
+ * 数组中的最大值
+ * @param {array} arr
+ * @example toolcore.maxNum([12,3,31,5,3]) // => 31 
+ */
+export const maxNum = (arr) =>
+    Math.max.apply(Math, arr)
+
+
+/**
+ * 数组中的最小值
+ * @param {array} arr 
+ * @example toolcore.minNum([12,3,31,5,3]) // => 3 
+ */
+export const minNum = (arr) =>
+    Math.min.apply(Math, arr)
