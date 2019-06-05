e4f308bb24830ac7370aa602d4bf0c5d15adace0
Benson <sucaiweb@163.com>
2019-06-05 14:42:11 +0800

    feat: base64加密解密函数

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import { version } from './package.json'
 import * as types from './src/types'
+import * as base64 from './src/base64'
 import * as obj from './src/object'
 import * as arr from './src/array'
 
 const toolcore = {
   ...types,
+  ...base64,
   ...obj,
   ...arr,
   version: '0.1.0'
