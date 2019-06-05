a0c601b0c52d1baf9e76b978f718012202775599
Benson <sucaiweb@163.com>
2019-06-05 14:57:08 +0800

    feat: 全部文件引入

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 import { version } from './package.json'
 import * as types from './src/types'
 import * as base64 from './src/base64'
-import * as obj from './src/object'
-import * as arr from './src/array'
+import * as array from './src/array'
+import * as date from './src/date'
+import * as fn from './src/function'
+import * as number from './src/number'
+import * as object from './src/object'
+import * as other from './src/other'
+import * as prototype from './src/prototype'
+import * as string from './src/string'
+import * as url from './src/url'
+import * as validator from './src/validator'
+
 
 const toolcore = {
   ...types,
   ...base64,
-  ...obj,
-  ...arr,
+  ...array,
+  ...date,
+  ...fn,
+  ...number,
+  ...object,
+  ...other,
+  ...prototype,
+  ...string,
+  ...url,
+  ...validator,
   version: '0.1.0'
   // version
 }
